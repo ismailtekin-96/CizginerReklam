f529d7e5546c614502e4951a7aa517db9fbd5413
mr.alone_26 <mr.alone_26@DESKTOP-TPQI028>
2022-09-06 13:05:24 +0300

    Add project files.

---
+++ CizginerReklam/Controllers/AnasayfaController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CizginerReklam.Controllers
+{
+    public class AnasayfaController : Controller
+    {
+        // GET: Anasayfa
+        public ActionResult anasayfa()
+        {
+            return View();
+        }
+        public PartialViewResult PartialSlider1()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialSlider2()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialSlider3()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ CizginerReklam/web-sablon/js/script1.js
@@ -0,0 +1,17 @@
+﻿var i = 0;
+var images = ['/web-sablon/canyon.jpg/', '/web-sablon/fransayaveda.jpg','pubgh.jpeg']
+
+var duration = 3000;
+function slideImg() {
+    document.slide.src = images[i];
+
+    if (i < images.length - 1) {
+        i++;
+    }
+    else {
+        i = 0;
+    }
+    setTimeout("slideImg()", duration);
+}
+window.onload = slideImg;
+
